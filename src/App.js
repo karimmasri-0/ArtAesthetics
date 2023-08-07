0d6aa4c4d7676d614f68b8838eb2e335e5436daa
XKESAR <karimmasri52@gmail.com>
2023-08-07 14:46:46 +0300

    improved error handling on front end

--- src/App.js
+++ src/App.js
@@ -5,53 +5,55 @@ import temp from "./images/soundcloud.png";
 import * as Yup from "yup";
 import { SlClose } from "react-icons/sl";
 import { ImSpinner5 } from "react-icons/im";
-// import { ImSpinner2 } from "react-icons/im";
-import { Buffer } from "buffer";
 
 function App() {
   const [image, setImage] = useState();
   const [imageTitle, setImageTitle] = useState();
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState({ error: false, text: "Downloading" });
-  const urlRegex = /^https?:\/\/(?:www\.)?soundcloud\.com\//;
+  const [scError, setScError] = useState(false);
+  const urlRegex = /^https?:\/\/(?:www\.)?(?:m\.)?(?:on\.)?soundcloud\.com\//;
 
   function base64ToUint8Array(base64) {
+    const start = performance.now();
     const binaryString = atob(base64);
     const length = binaryString.length;
     const uint8Array = new Uint8Array(length);
     for (let i = 0; i < length; i++) {
       uint8Array[i] = binaryString.charCodeAt(i);
     }
+    console.log(performance.now() - start);
     return uint8Array;
   }
-  const decodeBase64 = (data) => {
-    return Buffer.from(data, "base64").toString("ascii");
-  };
+
   const formik = useFormik({
     initialValues: {
       link: "",
     },
+    validateOnChange: false,
+    validateOnBlur: false,
     validationSchema: Yup.object({
       link: Yup.string()
         .matches(urlRegex, () => {
-          setError({ error: true, text: "Enter a valid url" });
+          formik.setErrors({ link: "Enter a valid url" });
+          setScError(true);
+          setIsPopoverOpen(true);
           return "Enter a valid url";
         })
         .required(() => {
-          setError({ error: true, text: "Link required" });
+          formik.setErrors({ link: "Link required" });
+          setIsPopoverOpen(true);
           return "Link required";
         }),
     }),
     onSubmit: (values) => {
       setIsLoading(true);
       axios
-        .post("http://localhost:5000/process_data", { link: values.link })
+        .post("http://192.168.1.107:5000/process_data", { link: values.link })
         .then(async (response) => {
           console.log(response.data);
           setImageTitle(response.data.title);
           if (response.data.src) {
-            console.log(typeof base64ToUint8Array(response.data.image_blob));
             const file = new File(
               [base64ToUint8Array(response.data.image_blob)],
               response.data.title,
@@ -72,7 +74,8 @@ function App() {
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
-            setError({ error: false, text: "Downloading" });
+            formik.setErrors({ link: "" });
+            setScError(false);
             setIsPopoverOpen(true);
             setIsLoading(false);
           }
@@ -80,9 +83,10 @@ function App() {
         .catch((error) => {
           setImage();
           setImageTitle();
+          formik.setErrors({ link: error.response.data.error });
+          setScError(true);
           setIsPopoverOpen(true);
           setIsLoading(false);
-          setError({ error: true, text: error.response.data.error });
           console.error(error);
         });
     },
@@ -91,8 +95,12 @@ function App() {
     if (isPopoverOpen)
       setTimeout(() => {
         setIsPopoverOpen(false);
+        setTimeout(() => {
+          formik.setErrors({ link: "" });
+        }, 333);
       }, 5000);
-  });
+  }, [isPopoverOpen]);
+
   return (
     <main className="mb-8 selection:text-red-400 selection:bg-gray-900 bg-cred">
       <div className="flex flex-col items-center pt-4 background">
@@ -104,14 +112,14 @@ function App() {
         </header>
         <div
           className={`mt-16 top-0 px-8 py-2 text-sm transition-all duration-500 border rounded-md  border-rose-400 opacity-0 ${
-            isPopoverOpen || formik.errors.link ? "opacity-100" : "opacity-0"
+            isPopoverOpen ? "opacity-100" : "opacity-0"
           } ${
-            error.error || formik.errors.link
+            formik.errors.link
               ? "text-cred bg-rose-300"
               : "text-rose-300 bg-gray-300/20"
           }`}
         >
-          {error.text}
+          {formik.errors.link ? formik.errors.link : "Downloading"}
         </div>
         <form
           onSubmit={formik.handleSubmit}
@@ -121,12 +129,12 @@ function App() {
             value={formik.values.link}
             onChange={formik.handleChange("link")}
             onBlur={formik.handleBlur("link")}
-            className="w-full px-1 mx-2 focus:outline-cred focus:ring-0 text-cred"
+            className="w-full px-0.5 mx-2 focus:outline-cred focus:ring-0 text-cred"
             placeholder="https://soundcloud.com/1812922/afraid-of-everything"
           />
           <button
             disabled={isLoading}
-            className={`w-1/4 my-0.5 inset-y-0 px-2 py-2.5 bg-cred right-1 rounded-xl hover:bg-red-900 ${
+            className={`w-28 my-0.5 inset-y-0 py-2.5 bg-cred right-1 rounded-xl hover:bg-red-900 ${
               isLoading && "cursor-not-allowed"
             }`}
             type="submit"
@@ -139,14 +147,15 @@ function App() {
           </button>
         </form>
       </div>
-      {(image || (error.error && !formik.errors.link)) && (
+
+      {(image || formik.errors.link) && !scError && (
         <div className="flex flex-col w-10/12 gap-6 p-6 mx-auto mt-10 rounded-lg shadow-xl sm:p-10 sm:w-8/12 md:w-8/12 lg:w-6/12 xl:w-4/12">
           <div
             className={`mx-8 font-semibold flex justify-center text-gray-300 sm:text-xl md:text-2xl ${
-              error.error ? "order-last" : "order-first"
+              formik.errors.link ? "order-last" : "order-first"
             }`}
           >
-            {error.error ? (
+            {formik.errors.link ? (
               <SlClose size={30} className="text-red-900" />
             ) : (
               imageTitle
@@ -154,9 +163,9 @@ function App() {
           </div>
           <img
             alt="album art"
-            src={error.error ? temp : image}
+            src={scError ? temp : image}
             className={`${
-              image || error.error
+              true
                 ? "w-full transition-all duration-700 scale-100 rounded border-zinc-900"
                 : "w-0 scale-0"
             } ${image && " shadow-xl"}`}
